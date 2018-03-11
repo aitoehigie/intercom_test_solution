f16c6de1f4c4d6bfd5c2286b3470b11b234d54c4
aitoehigie <aitoehigie@gmail.com>
2018-03-11 01:03:00 +0100

    create a app_logic method and file_opener split from a single monolithic method 'main'

--- application.py
+++ application.py
@@ -25,37 +25,44 @@ def distance_calculator(slat, slong, elat, elong):
 
 
 def file_reader(file_name):
-    customers = []
     try:
-        with open(file_name, "r") as file:
-            for line in file.readlines():        
-                json_data = json.loads(line)
-                elat = float(json_data.get("latitude"))
-                elong = float(json_data.get("longitude"))
-                distance = distance_calculator(slat=INTERCOM_LATITUDE, slong=INTERCOM_LONGITUDE, elat=elat, elong=elong)
-                if distance <= 100:
-                    customers.append(dict(name = json_data.get("name"), user_id = json_data.get("user_id")))                
-                else:
-                    continue
+        file_object =  open(file_name, "r")
+        return file_object
     except IOError as e:
         print ("I/O error({0}): {1}".format(e.errno, e.strerror))
     except:
         print ("Unexpeceted error:", sys.exc_info()[0])
+
+
+def app_logic(file_name):
+    customers = []
+    file_object = file_reader(file_name)
+    for line in file_object.readlines():
+        json_data = json.loads(line)
+        elat = float(json_data.get("latitude"))
+        elong = float(json_data.get("longitude"))
+        distance = distance_calculator(slat=INTERCOM_LATITUDE, slong=INTERCOM_LONGITUDE, elat=elat, elong=elong)
+        if distance <= 100:
+            customers.append(dict(name = json_data.get("name"), user_id = json_data.get("user_id")))
+        else:
+            continue
+    file_object.close()
     customers = sorted(customers, key=lambda customers: customers["user_id"])
     return customers
 
+
 @click.command()
-@click.argument("file_name")
+@click.option("--file-name", "-f", help="Enter the name of the json file.",)
 def main(file_name):
     """
    This is little program that will read the full list of customers and output
    the names and user ids of matching customers (within 100km) of Intercom Dublin offices, sorted by User ID (ascending).
-   Why? They are good guys and want to invite them for lunch. :)
+   Why? They are good guys and want to invite them for some food and drinks. :)
 
    It is compulsory while calling the program that you pass in a json file containing the customer details.
    """
     pp = pprint.PrettyPrinter(indent=4)
-    return pp.pprint(file_reader(file_name))
+    return pp.pprint(app_logic(file_name))
 
 if __name__ == "__main__":
     main()
